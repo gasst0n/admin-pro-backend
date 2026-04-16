36498398989578ba9fc17e6bab7e7fac7748180d
Gaston Godoy <gasst0n14@gmail.com>
2026-04-16 19:47:33 -0300

    AngularApp

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 
 // CORS
 const cors = require("cors");
@@ -37,6 +38,12 @@ app.use("/api/upload", require("./routes/uploads"));
  * En local usa 3000
  */
 const PORT = process.env.PORT || 3000;
+
+// LO ULTIMO
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto", PORT);
